bd794e618ab2fbfd1e8ca46e1b5f6a229d231893
tamura <tomoki0927.7939@icloud.com>
2024-12-30 23:39:18 +0900

    Servermodel:migrate

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_28_141316) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_30_142822) do
+  create_table "servers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "icon_url"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_servers_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -20,4 +29,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_28_141316) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "servers", "users"
 end
