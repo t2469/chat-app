3115e9d1da2eed8190b25d8c1fd3f1944e3aecbc
tamura <tomoki0927.7939@icloud.com>
2025-01-03 23:33:14 +0900

    推奨されていない書き方を修正

--- backend/app/controllers/api/authentication_controller.rb
+++ backend/app/controllers/api/authentication_controller.rb
@@ -28,7 +28,7 @@ module Api
     end
 
     def encode_token(payload)
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      JWT.encode(payload, Rails.application.credentials.secret_key_base)
     end
 
   end
