a64dc519916d0c93138d4f6a7b4ad84334b3c05b
tamura <tomoki0927.7939@icloud.com>
2025-01-04 15:35:18 +0900

    関連付けを修正

--- backend/app/models/server.rb
+++ backend/app/models/server.rb
@@ -1,6 +1,6 @@
 class Server < ApplicationRecord
   has_many :server_members, dependent: :destroy
-  has_many :members, through: :server_members
+  has_many :users, through: :server_members
 
   validates :name, presence: true
 end
