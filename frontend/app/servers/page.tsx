9d957ad3cb4eec65f7dc4fee56e8ec2e463ba055
tamura <tomoki0927.7939@icloud.com>
2025-01-09 00:25:21 +0900

    generate controller channels

---
+++ frontend/app/servers/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react';
+
+export default function ServersPage() {
+    return (
+        <div className="p-6 bg-gray-900 h-full flex items-center justify-center text-white">
+            <h1 className="text-2xl">サーバーを選択してください</h1>
+        </div>
+    );
+}
