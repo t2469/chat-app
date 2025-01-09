fb45a332c7035ad559d183d7d3eec9c7a1a58a56
tamura <tomoki0927.7939@icloud.com>
2025-01-10 04:55:41 +0900

    docker環境の修正

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000/api',
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
 api.interceptors.request.use(
