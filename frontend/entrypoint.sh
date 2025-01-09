122ddf50b7238e2d28d112ac985557c993154c45
tamura <tomoki0927.7939@icloud.com>
2025-01-10 04:35:27 +0900

    dockerコンテナが閉じてしまう問題を解決

---
+++ frontend/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ "$APP_ENV" = "production" ]; then
+  echo "Running in production mode..."
+  npm run build
+  npm run start
+else
+  echo "Running in development mode..."
+  npm run dev
+fi
+
