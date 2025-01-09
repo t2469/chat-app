f1de0e5a75078051c3258cc242d4e85860f3857e
tamura <tomoki0927.7939@icloud.com>
2025-01-10 04:59:49 +0900

    application_controllerの推奨されていないを修正

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::API
     header = request.headers['Authorization']
     header = header.split(' ').last if header.present?
     begin
-      decoded = JWT.decode(header, Rails.application.secrets.secret_key_base)[0].with_indifferent_access
+      decoded = JWT.decode(header, Rails.application.credentials.secret_key_base)[0].with_indifferent_access
       @current_user = User.find(decoded[:user_id])
     rescue ActiveRecord::RecordNotFound => e
       render json: { errors: e.message }, status: :unauthorized
