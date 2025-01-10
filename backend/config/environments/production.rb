5972c63bc42d0d241cc84e9146fec2eef1684605
tamura <tomoki0927.7939@icloud.com>
2025-01-10 19:51:04 +0900

    railsのproduction.rbのcableのURを修正L

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   config.eager_load = true
   config.consider_all_requests_local = false
   config.force_ssl = false
-  config.action_cable.url = "ws://13.114.232.178/cable"
+  config.action_cable.url = "ws://13.114.232.178:3000/cable"
   config.action_cable.allowed_request_origins = ['http://13.114.232.178:8000', 'https://13.114.232.178:8000']
   config.hosts << "13.114.232.178"
 end
