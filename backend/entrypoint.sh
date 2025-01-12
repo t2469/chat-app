f2d8b8d4962f1a17cec0e5bf6dd4af2f6e87394f
tamura <tomoki0927.7939@icloud.com>
2025-01-12 22:49:59 +0900

    railsで本番環境用のファイルが使用されていない問題を修正

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# APP_ENV が設定されていない場合は 'development' をデフォルトとする
+export APP_ENV=${APP_ENV:-development}
+
+# APP_ENV の値を RAILS_ENV と RACK_ENV にコピー
+export RAILS_ENV=${APP_ENV}
+export RACK_ENV=${APP_ENV}
+
 # RailsサーバーのPIDファイルを削除
 rm -f /app/tmp/pids/server.pid
 
