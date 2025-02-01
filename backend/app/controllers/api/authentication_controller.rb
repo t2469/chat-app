3ec9e8380201f4bf5dfc21730e7665acf21cdebb
tamura <tomoki0927.7939@icloud.com>
2025-02-01 23:17:54 +0900

    feat #1 バックエンドでのemailの使用箇所を変更

--- backend/app/controllers/api/authentication_controller.rb
+++ backend/app/controllers/api/authentication_controller.rb
@@ -13,18 +13,18 @@ module Api
     end
 
     def login
-      user = User.find_by(email: params[:email])
+      user = User.find_by(username: params[:username])
       if user&.authenticate(params[:password])
         token = encode_token(user_id: user.id)
         render json: { token: token, user: user }, status: :ok
       else
-        render json: { error: 'Invalid email or password' }, status: :unauthorized
+        render json: { error: 'Invalid username or password' }, status: :unauthorized
       end
     end
 
     private
     def user_params
-      params.require(:user).permit(:username, :email, :password, :password_confirmation, :avatar_url)
+      params.require(:user).permit(:username, :password, :password_confirmation, :avatar_url)
     end
 
     def encode_token(payload)
