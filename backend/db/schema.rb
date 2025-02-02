75f103cfd9130ada1399d3adb491f635b86e67d7
tamura <tomoki0927.7939@icloud.com>
2025-02-02 23:04:43 +0900

    feat: #1 db:migrate

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_02_01_125754) do
-  create_table "channels", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+  create_table "channels", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_01_125754) do
     t.index ["server_id"], name: "index_channels_on_server_id"
   end
 
-  create_table "messages", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+  create_table "messages", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "content"
     t.bigint "channel_id"
     t.datetime "created_at", null: false
@@ -28,7 +28,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_01_125754) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
-  create_table "server_members", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+  create_table "server_members", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "server_id", null: false
     t.string "role"
@@ -38,13 +38,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_01_125754) do
     t.index ["user_id"], name: "index_server_members_on_user_id"
   end
 
-  create_table "servers", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+  create_table "servers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
     t.datetime "created_at", null: false
